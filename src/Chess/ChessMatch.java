b172e3b2becd57afad2f48ad9d608b603ae2c877
Osvaldo <osvaldo.junior13@gmail.com>
2024-08-14 15:37:11 -0300

    criadas peças e começando a colocalas no tabuleiro

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package Chess;
 
 import BoardGame.Board;
+import BoardGame.Position;
+import Chess.pieces.King;
+import Chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -9,6 +12,7 @@ public class ChessMatch {
     public ChessMatch(){
 
         board = new Board(8,8);
+        initialSetUp();
 
     }
     public ChessPiece[][] getPieces(){
@@ -20,6 +24,11 @@ public class ChessMatch {
 
         }
         return mat;
+    }
+    private void initialSetUp(){
+        board.placePiece(new Rook(board, Color.WHITE),new Position(2,1));
+        board.placePiece(new King(board, Color.BLACK),new Position(0,4));
+        board.placePiece(new King(board, Color.WHITE),new Position(7,4));
     }
 
 }
