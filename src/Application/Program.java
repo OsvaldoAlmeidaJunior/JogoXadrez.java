41956687e6f60535a3a86f43ec8082e4e0dce16d
Osvaldo <osvaldo.junior13@gmail.com>
2024-08-16 16:41:39 -0300

    criada movimentação de peças

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -2,12 +2,25 @@ package Application;
 
 
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
-
+        while (true){
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+            System.out.println();
+            System.out.print("target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+            ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+        }
     }
 }
   
--- src/Chess/ChessException.java
+++ src/Chess/ChessException.java
@@ -1,6 +1,8 @@
 package Chess;
 
-public class ChessException extends RuntimeException{
+import BoardGame.BoardException;
+
+public class ChessException extends BoardException{
     private static final long seriaLVersionUID = 1L;
 
     public ChessException(String msg){
