6636a56dae55c33219bb1eac9f4c15800da55ab2
Osvaldo <osvaldo.junior13@gmail.com>
2024-08-19 14:49:33 -0300

    criado método para verificar movimentos possívesi de uma peça

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -44,6 +44,10 @@ public class ChessMatch {
         if(!board.therIsAPiece(position)){
             throw new ChessException("Não existe peça na posição de origem");
         }
+        if(!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("Não existem movimentos possíveis para esta peça");
+
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
